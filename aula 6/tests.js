9d9238954b7e8b4a05e45452cd333d5869607cad
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-11 15:52:28 -0300

    Update tests.js

--- aula 6/tests.js
+++ aula 6/tests.js
@@ -1,5 +1,7 @@
-console.log('João nasceu em 1984');
-console.log('Em 2000 João conheceu Maria');
-console.log('João casou-se com Maria em 2012');
-console.log('Maria teve 1 filho com João em 2015');
-console.log('O filho de Joã se chama Cleberson');
+let nome = 'João'; //String
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de',nome , 'se chama Cleberson');
