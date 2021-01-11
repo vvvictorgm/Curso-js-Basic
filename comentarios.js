16efa791a426dc973a0b1e9972b2251d997f15c5
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-11 13:43:47 -0300

    Comentarios em JS

---
+++ comentarios.js
@@ -0,0 +1,14 @@
+//comentarios em js
+console.log('Olá mundo!');//comentario
+console.log('Linha1');
+//comentario
+
+/*
+um bloco de comentario
+pode ser utilizado entre varias
+eu já sabia disso, mas vou aprender do zero, vai que...
+*/
+console.log('Linha2');
+//console.log('Linha3');
+console.log('Linha4');
+// outro comentario
