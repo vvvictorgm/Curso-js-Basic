2f41aff6d8f1fa1ffd6d0c372f3a9de1478ac3ed
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-12 19:42:51 -0300

    Update index.js

--- aula 9/index.js
+++ aula 9/index.js
@@ -6,4 +6,13 @@ const num = 10; //number
 const num2 = 10.52 //number
 let nomeAluno //undefined = não aponta para nenhum lugar a propria linguagem irá setar isso, nunca o programador
 const sobrenomeAluno = null // Nulo -> desconfigurar uma variavel
-const boolean = true; //true or false 
+const boolean = true; //true or false
+
+const array = [1,2];
+const b = array;
+
+console.log(array,b);
+
+b.push(3);
+
+console.log(array,b); 
