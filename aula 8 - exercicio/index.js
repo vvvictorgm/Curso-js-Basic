b909295fc5fcb463601b15c9214c1282d6efc236
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-12 15:40:28 -0300

    nova forma de exibir variaveis

--- aula 8 - exercicio/index.js
+++ aula 8 - exercicio/index.js
@@ -15,5 +15,5 @@ let anoNascimento = 2021 - idade;
 indiceMassaCorporal = peso/(altura*altura);
 
 console.log(nome, sobrenome, 'tem', idade, 'anos, pesa',peso,'kg');
-console.log('tem', altura, 'de altura e seu IMC é de', indiceMassaCorporal);
+console.log(`tem ${altura} de altura e seu IMC é de ${indiceMassaCorporal}`);
 console.log(nome, 'nasceu em', anoNascimento);
