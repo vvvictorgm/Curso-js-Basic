7f0d80d811151b928d6b2aca7d13bcfb57b200b4
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-12 20:03:31 -0300

    Update index.js

--- aula 10 - operadores aritméticos/index.js
+++ aula 10 - operadores aritméticos/index.js
@@ -17,6 +17,23 @@
  console.log(num1**num2);
  // %
  console.log(num1%num2);
- 
+
+//incrimento
+ let contador = 1;
+ contador ++;
+ ++contador;
+ console.log(contador);
+console.log(contador++);
+console.log(contador);
+console.log(++contador);
+
+
+
+ //decremento
+ let contadornegativo = 1;
+ contadornegativo --;
+ --contadornegativo;
+ console.log(contadornegativo);
+
 
  
