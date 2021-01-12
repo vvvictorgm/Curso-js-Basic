ade4c6ecf1b8492dcd6311d99416af571c292281
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-12 15:32:57 -0300

    Update index.js

---
+++ aula 8 - exercicio/index.js
@@ -0,0 +1,14 @@
+/*
+Luiz Otavio Mirando tem 30 anos, pesa 84kg
+tem 1.8 de altura e seu IMC é de 25.9595959595954
+Luiz Otavio nasceu em 198x
+*/
+const nome = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const altura = 1.80;
+
+let imc = peso/(altura*altura);
+let anoNascimento = idade - 2021;
+
