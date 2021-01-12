760484c0d3ef3982e44bf7b2735ea4a1a1fb3168
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-12 15:21:51 -0300

    Update constantes.js

--- aula 7/constantes.js
+++ aula 7/constantes.js
@@ -1,7 +1,9 @@
 const nome = 'Victor';
 console.log(nome);
 
-const primeiroNumero = 5;
+const primeiroNumero = 5; //number
 const segundoNumero = 10;
 const conta = primeiroNumero * segundoNumero;
 console.log(conta);
+console.log(typeof primeiroNumero);
+
