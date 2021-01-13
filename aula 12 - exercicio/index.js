88a0cef19cc13779c5500a0abf975985133287d1
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-13 16:23:58 -0300

    Update index.js

--- aula 12 - exercicio/index.js
+++ aula 12 - exercicio/index.js
@@ -1,13 +1,16 @@
 let varA = 'A'; //B
 let varB = 'B'; //C
 let varC = 'C'; //A
-
+/*correto
 let varSobra = varA;
 
 varA = varB;
 varB = varC;
 varC = varSobra;
+*/
 
+/*professor*/
+[varA, varB, varC] = [varB, varC, varA];
 console.log(varA,varB,varC);
 
 
