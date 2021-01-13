9dbee23de51b2fe5cad7b2f5f1b01396a1f25e88
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-13 16:37:20 -0300

    Update index.js

--- aula 13/index.js
+++ aula 13/index.js
@@ -1,5 +1,8 @@
 let umaString = 'Um texto';
 
-console.log(umaString.concat(' em um lindo dia.'));
+/*console.log(umaString.concat(' em um lindo dia.'));
 console.log(umaString + ' em um lindo dia.');
 console.log(`${umaString} em um lindo dia.`);
+*/
+
+console.log(umaString.indexOf('texto'));
