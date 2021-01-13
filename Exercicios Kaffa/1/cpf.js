36217e6b409f0bd1d32c2eebdad12338730d77c8
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-13 18:22:47 -0300

    Update cpf.js

--- Exercicios Kaffa/1/cpf.js
+++ Exercicios Kaffa/1/cpf.js
@@ -1,8 +1,12 @@
+//the information on what criteria are needed to validate a CPF was taken from the IRS website
+
 let cpf = 10;
 console.log(cpf.length != 11)
+
 function verificarCPF(cpf){
-    if(cpf.length != 11){
-        
+//first: we will try if the cpf has less than 11 characters, and see if they aren't 111, 222, 333 etc...
+    if(cpf.length != 11 ){
+        //alert('cpf invalido');
     }
 
 }
