7f4b01c424f2f6ebc3197bc5923b455da2b33957
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-13 16:42:41 -0300

    Update index.js

--- aula 13/index.js
+++ aula 13/index.js
@@ -5,6 +5,18 @@ console.log(umaString + ' em um lindo dia.');
 console.log(`${umaString} em um lindo dia.`);
 */
 
+/*indexof
 console.log(umaString.indexOf('texto'));
 console.log(umaString.lastIndexOf('Um' , 3));
 console.log(umaString.indexOf('Um' , 3));
+*/
+
+//expressões regulares
+console.log(umaString.match(/[a-z]/g));
+console.log(umaString.search(/[a-z]/));
+console.log(umaString.replace('Um', 'Outra'));
+
+let OutraString = 'O rato roeu a roupa do rei de roma.';
+
+console.log(OutraString.replace(/r/, '#'));
+console.log(OutraString.replace(/r/g, '#'));
