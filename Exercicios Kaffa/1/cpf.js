a70de415ed9fdb4793a51499bbdb2ed3b693b668
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-13 18:28:10 -0300

    validando os primeiros pontos

--- Exercicios Kaffa/1/cpf.js
+++ Exercicios Kaffa/1/cpf.js
@@ -1,12 +1,28 @@
 //the information on what criteria are needed to validate a CPF was taken from the IRS website
 
-let cpf = 10;
-console.log(cpf.length != 11)
+let cpfTeste = 10;
 
 function verificarCPF(cpf){
-//first: we will try if the cpf has less than 11 characters, and see if they aren't 111, 222, 333 etc...
-    if(cpf.length != 11 ){
-        //alert('cpf invalido');
+    //in this point we will remove all points and expressions in the cpf, if it has
+    cpf = cpf.replace(/[^\d]+/g,''); 
+    if(cpf == ''){
+        return false;
+    }   
+
+//first: if the cpf has less than 11 characters, and see if they aren't 111, 222, 333 etc...
+    if(cpf.length != 11 ||
+    cpf == "00000000000" ||       
+    cpf == "11111111111" ||         
+    cpf == "22222222222" ||         
+    cpf == "33333333333" ||         
+    cpf == "44444444444" ||         
+    cpf == "55555555555" ||         
+    cpf == "66666666666" ||         
+    cpf == "77777777777" ||         
+    cpf == "88888888888" ||         
+    cpf == "99999999999"){
+        return false;
     }
+
 
 }
