5cbcf456da186b33314347f1e6ba683ba4ecfa4b
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-13 16:38:35 -0300

    Update index.js

--- aula 13/index.js
+++ aula 13/index.js
@@ -6,3 +6,5 @@ console.log(`${umaString} em um lindo dia.`);
 */
 
 console.log(umaString.indexOf('texto'));
+console.log(umaString.lastIndexOf('Um' , 3));
+console.log(umaString.indexOf('Um' , 3));
