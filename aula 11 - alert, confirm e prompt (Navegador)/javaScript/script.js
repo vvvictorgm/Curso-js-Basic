894cd67f0bb91dc81760f54ef248e86881f87f95
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-13 16:10:42 -0300

    Update script.js

--- aula 11 - alert, confirm e prompt (Navegador)/javaScript/script.js
+++ aula 11 - alert, confirm e prompt (Navegador)/javaScript/script.js
@@ -1,4 +1,4 @@
 let num1 = Number(prompt('digte um numero'));
 let num2 = Number(prompt('digite outro numero'));
 let resultado = num1+num2;
-alert('o resultado foi:'+ resultado);
+alert(`o resultado foi: ${resultado}`);
