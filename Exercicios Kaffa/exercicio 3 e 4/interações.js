dfc832555b93326fe5e3542639105808cc68e378
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-15 18:23:07 -0300

    pensando nas ultimas contas

--- Exercicios Kaffa/exercicio 3 e 4/interações.js
+++ Exercicios Kaffa/exercicio 3 e 4/interações.js
@@ -3,19 +3,26 @@ let retangulo = [7,2,13,7];
 let retangulo2 = [11,11,15,13];
 
 function intersec(rec1,rec2){
-    let r1_overlap,r2_overlp;
+    
     //make the first rectangle
     let x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3]; 
     //make the secound one
     let x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
-    
-    // Question 4
-    //first we have to compute the area of retangles
-    r1_overlap = Math.abs(x1 - y1) * Math.abs(x2 - y2);
-    r2_overlp = Math.abs(x3 - y3) * Math.abs(x4 - y4);
 
+    //bottom left point of the overlapping area.
+	let bl_x = Math.max(x1, x3);
+	let bl_y = Math.max(y1, y3);
+
+	// top right point of the overlapping area.
+	let tr_x = Math.min(x2, x4);
+    let tr_y = Math.min(y2, y4);
     
-    console.log(r1_overlap);
+    let total = (tr_x - bl_x) * (tr_y - bl_y);
+    console.log(total);
+
+ 
+	
+
     //we know the rectangles are overlapping if:
     if((x1 < x4) && (x3 < x2) && (y1 < y4) && (y3 < y2)){
         return true;
@@ -27,10 +34,19 @@ function intersec(rec1,rec2){
         return false;
     }
 
+}
+function areaDosRetangle(rec1,rec2){
+    let r1_overlap,r2_overlp;
+// Question 4
+    //first we have to compute the area of retangles
+    r1_overlap = Math.abs(x1 - y1) * Math.abs(x2 - y2);
+    r2_overlp = Math.abs(x3 - y3) * Math.abs(x4 - y4);
+    return(r1_overlap,r2_overlp);
+}
+function areaEmInterccao(){
     
 
-    return true;
-
 }
+
 console.log(intersec(retangulo,retangulo2));
 
