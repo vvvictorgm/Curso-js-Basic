b1c72689b3fdfae8bc9723de81abd08466e58ba0
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-15 15:22:14 -0300

    Update interações.js

--- Exercicios Kaffa/exercicio 3 e 4/interações.js
+++ Exercicios Kaffa/exercicio 3 e 4/interações.js
@@ -1,3 +1,17 @@
-let rectA = [[3,5][11,11]];
-let rectB = [[7,2][13,7]];
-let rectC = [[11,11][15,13]];
+//create the retangles
+
+let a1,a2,a3,a4,b1,b2,b3,b4,c1,c2,c3,c4;
+let rectA = [[a1,a2][a3,a4]];
+let rectB = [[b1,b2][b3,b4]];
+let rectC = [[c1,c2][c3,c4]];
+
+/*find the new 2 points,
+we have the width and height of the points 1 and 2
+we have to discover width and height of the points 3 and 4
+the rule for the first two pointos is
+w1,h1 || w2,h2
+we can observe the other two points is
+w1,h2 || w2,h1
+*/
+
+let reactANewPoints = [[reactA[a1,a4]][a2,a3]];
