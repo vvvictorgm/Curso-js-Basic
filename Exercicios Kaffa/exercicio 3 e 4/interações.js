7c33e2c8954fb1caf565529fac8f4ca63aa04754
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-15 17:52:16 -0300

    achamos a area

--- Exercicios Kaffa/exercicio 3 e 4/interações.js
+++ Exercicios Kaffa/exercicio 3 e 4/interações.js
@@ -3,16 +3,19 @@ let retangulo = [7,2,13,7];
 let retangulo2 = [11,11,15,13];
 
 function intersec(rec1,rec2){
+    let r1_overlap,r2_overlp;
     //make the first rectangle
     let x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3]; 
     //make the secound one
     let x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
+    
     // Question 4
-    //first we have to compute the area of retangle are overlape
-    let x_overlap,y_overlp;
-    //than we do the math:
-    x_overlap = ((x1+y1)*(x3+y3))-(x1)*(x3);
-    console.log(x_overlap);
+    //first we have to compute the area of retangles
+    r1_overlap = Math.abs(x1 - y1) * Math.abs(x2 - y2);
+    r2_overlp = Math.abs(x3 - y3) * Math.abs(x4 - y4);
+
+    
+    console.log(r1_overlap);
     //we know the rectangles are overlapping if:
     if((x1 < x4) && (x3 < x2) && (y1 < y4) && (y3 < y2)){
         return true;
