4078da65926848b3d8024c4c90293768b3eaa253
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-15 19:20:14 -0300

    Update interações.js

--- Exercicios Kaffa/exercicio 3 e 4/interações.js
+++ Exercicios Kaffa/exercicio 3 e 4/interações.js
@@ -1,5 +1,5 @@
 //create the retangles
-let retangulo = [3,5,11,11];
+let retangulo = [0,0,1,1];
 let retangulo2 = [7,2,13,7];
 
 function intersec(rec1,rec2){
@@ -9,28 +9,31 @@ function intersec(rec1,rec2){
     //make the secound one
     let x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
 
-    //bottom left point of the overlapping area.
-	let bl_x = Math.max(x1, x3);
-	let bl_y = Math.max(y1, y3);
-
-	// top right point of the overlapping area.
-	let tr_x = Math.min(x2, x4);
-    let tr_y = Math.min(y2, y4);
+    /*let xIntersec = (Math.min(x2, x4)-Math.max(x1, x3));
     
-    let total = (tr_x - bl_x) * (tr_y - bl_y);
-    let area1 = Math.abs(x1 - y1) * Math.abs(x2 - y2);
-    let area2 = Math.abs(x3 - y3) * Math.abs(x4 - y4);
-    let areaTotalInterccao = area1+area2-total;
-    console.log(areaTotalInterccao);
+	let yIntersec = (Math.min(y2, y4) - Math.max(y1, y3));
+    
+    //let totalIntersec = xIntersec * yIntersec;
+    */
+
+    let areaTest = ((y2-y1+1)*(x2-x1+1));
+    let areaTest2 = ((y2-y1+1)*(x2-x1+1));
+    let area1 = ((x2-x1)*(y2-y1));
+    let area2 = ((x4-x3)*(y4-y3));
+
+    //let areaTotalInterccao = area1+area2-totalIntersec;
+
+    console.log(areaTest);
+
 
 
      //we know the rectangles are overlapping if:
-    if((x1 < x4) && (x3 < x2) && (y1 < y4) && (y3 < y2)){
+    if((x1 < x4) && (x3 < x2) && (y1 < y4) && (y3 < y2) 
+    //and if some point of the rectangles just touch in other, it's true too.
+    || x1 == x3 || x1 == x4 || x2 == x3 || x2 == x4 || y1 == y3 || y1 == y4 || y2 == y3 || y2 == y4){
         return true;
-        //and if some point of the rectangles just touch in other, it's true too.
-    }else if(x1 == x3 || x1 == x4 || x2 == x3 || x2 == x4 || y1 == y3 || y1 == y4 || y2 == y3 || y2 == y4 ){
-        return true;
-    }else{
+        
+        }else{
         //if any part of the rectangles touchs another rectangles, it's nothing touching at all
         return false;
     }
