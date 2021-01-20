ad26a084eca401936bae0165a33a83391b49cb3c
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-20 18:29:29 -0300

    comentando e tirando os console de teste

--- Exercicios Kaffa/exercicio 3 e 4/interações.js
+++ Exercicios Kaffa/exercicio 3 e 4/interações.js
@@ -11,22 +11,22 @@ function intersec(rec1, rec2) {
     let x1 = rec1[0], y1 = rec1[1], x2 = rec1[2], y2 = rec1[3];
     //make the secound one
     let x3 = rec2[0], y3 = rec2[1], x4 = rec2[2], y4 = rec2[3];
-
+    /*than we check all the possibilities when just 1 point is touching with another, 
+    this happen only when cornors touch    
+    */
     if (((x1 == x3 && y1 == y3) || (x2 == x3) && (y2 == y3) || (x1 == y3) && (y2 == x4) || (x2 == y3) && (y1 == x4))) {
         totalIntersec = 1;
     } else {
-        console.log(Math.min(x2, x4) - Math.max(x1, x3));
+        //when we exclude the possibility of the cornors touching, know we have to check if in fact the rectangles are intersect each others
         if ((Math.min(x2, x4) - Math.max(x1, x3)) > 0) {
             xIntersec = (Math.min(x2, x4) - Math.max(x1, x3)) + 1;
 } else {
-            console.log("eu passei pelo x0");
             xIntersec = 0;
         }
 
         if ((Math.min(y2, y4) - Math.max(y1, y3)) > 0) {
             yIntersec = (Math.min(y2, y4) - Math.max(y1, y3)) + 1;
 } else {
-            console.log("eu passei pelo y0");
             yIntersec = 0;
         }
 
