d8f60896020ece16928ded40fcae8d3b5d27a905
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-19 21:04:33 -0300

    falta testar

--- Exercicios Kaffa/exercicio 3 e 4/interações.js
+++ Exercicios Kaffa/exercicio 3 e 4/interações.js
@@ -1,8 +1,9 @@
 //create the retangles
 let retangulo = [3, 5, 11, 11];
-let retangulo2 = [11, 8, 14, 11];//i create, it's supose to be 4
+//let retangulo2 = [11, 8, 14, 11];//i create, it's supose to be 4
 //let retangulo2 = [7, 2, 13, 7];//b
 //let retangulo2 = [11,11,15,13];//c
+let retangulo2 = [0,0,1,1];
 
 function intersec(rec1, rec2) {
     let xIntersec, yIntersec, totalIntersec;
@@ -17,32 +18,32 @@ function intersec(rec1, rec2) {
         console.log(Math.min(x2, x4) - Math.max(x1, x3));
         if ((Math.min(x2, x4) - Math.max(x1, x3)) > 0) {
             xIntersec = (Math.min(x2, x4) - Math.max(x1, x3)) + 1;
-        } else if (x1 == x3 || x1 == x4 || x2 == x3 || x2 == x4) {
+        /*} else if (x1 == x3 || x1 == x4 || x2 == x3 || x2 == x4) {
             console.log("eu passei pelo xxx");
             xIntersec = testeDeEncostar(x1, x2, x3, x4);
-        } else {
+        */} else {
             console.log("eu passei pelo x0");
             xIntersec = 0;
         }
 
         if ((Math.min(y2, y4) - Math.max(y1, y3)) > 0) {
             yIntersec = (Math.min(y2, y4) - Math.max(y1, y3)) + 1;
-        } else if (y1 == y3 || y1 == y4 || y2 == y3 || y2 == y4) {
+        /*} else if (y1 == y3 || y1 == y4 || y2 == y3 || y2 == y4) {
             console.log("eu passei pelo y");
             yIntersec = testeDeEncostar(y1, y2, y3, y4);
 
-        } else {
+        */} else {
             console.log("eu passei pelo y0");
             yIntersec = 0;
         }
 
         if ((xIntersec == 0) || (yIntersec == 0)) {
-            totalIntersection = 0;
+            totalIntersec = xIntersec + yIntersec;
         } else {
             totalIntersec = xIntersec * yIntersec;
         }
     }
-    console.log(totalIntersec, yIntersec, xIntersec);
+    console.log(xIntersec, yIntersec, totalIntersec);
 
     //we know the rectangles are overlapping if:
     if ((x1 < x4) && (x3 < x2) && (y1 < y4) && (y3 < y2)
@@ -55,7 +56,7 @@ function intersec(rec1, rec2) {
     }
 
 }
-function testeDeEncostar(n1, m1, n2, m2) {
+/*function testeDeEncostar(n1, m1, n2, m2) {
     let a1 = n1;
     let a2 = m1;
     let a3 = n2;
@@ -86,7 +87,7 @@ function testeDeEncostar(n1, m1, n2, m2) {
 
     return (diferenca + 1);
 
-}
+}*/
 
 
 console.log(intersec(retangulo, retangulo2));
