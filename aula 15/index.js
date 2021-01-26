1f7f9dc366530ae1861c5cad475c4f6c0b3b2eab
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-26 18:45:33 -0300

    Update index.js

--- aula 15/index.js
+++ aula 15/index.js
@@ -3,3 +3,10 @@ let num2 = Math.floor(num1);
 let num3 = Math.ceil(num1);
 let num4 = Math.round(num1);
 console.log(num2, num3, num4);
+
+
+console.log(Math.max(1,2,3,4,5,6,-10,231,323,322,-140,1556));
+console.log(Math.min(1,2,3,4,5,6,-10,231,323,322,-140,1556));
+
+const aleatorio = Math.round(Math.random()*(10));
+console.log(aleatorio);
