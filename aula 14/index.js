4cd8ab1a13b7629f620057022afd38e621dda6e2
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-26 18:28:01 -0300

    Update index.js

--- aula 14/index.js
+++ aula 14/index.js
@@ -1,2 +1,6 @@
-let num1 = 1;
+let num1 = 1232121;
 let num2 = 2.5;
+
+//console.log(num1.toString() + num2); //utilizando o num 1 como String, mas ele continua sendo um numero
+//num1 = num1.toString(); //agora ele é uma string
+//console.log(num1.toString(2));//valor binario
