b2275b277f20fd473bdbf7cb6506b3d45b270d38
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-26 18:28:58 -0300

    Update index.js

--- aula 14/index.js
+++ aula 14/index.js
@@ -1,6 +1,7 @@
-let num1 = 1232121;
+let num1 = 10.548754883;
 let num2 = 2.5;
 
 //console.log(num1.toString() + num2); //utilizando o num 1 como String, mas ele continua sendo um numero
 //num1 = num1.toString(); //agora ele é uma string
 //console.log(num1.toString(2));//valor binario
+console.log(num1.toFixed(2));//arredonda para o numero de casa decimais definido na caixa 
