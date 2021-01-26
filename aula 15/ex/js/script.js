1d71e6816a4c41b96a38ae09573ef9fb39b18519
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-26 19:14:20 -0300

    exercicio finalizado

---
+++ aula 15/ex/js/script.js
@@ -0,0 +1,15 @@
+const numero = Number(prompt('Digite num numero'));
+const numeroTitulo = document.getElementById('numero-titulo');
+const texto = document.getElementById('texto');
+
+
+numeroTitulo.innerHTML = numero;
+
+texto.innerHTML = '';
+texto.innerHTML += `<p>Raiz quadrada: ${numero ** 0.5} </p>`
+texto.innerHTML += `<p>${numero} é inteiro: ${Number.isInteger(numero)} </p>`
+texto.innerHTML += `<p>É Nan?: ${Number.isNaN(numero)} </p>`
+texto.innerHTML += `<p>Arredondado para baixo: ${Math.floor(numero)} </p>`
+texto.innerHTML += `<p>Arredondado para cima: ${Math.ceil(numero)} </p>`
+texto.innerHTML += `<p>Com duas casas decimais: ${numero.toFixed(2)} </p>`
+
