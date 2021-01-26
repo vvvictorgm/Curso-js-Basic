25d18b0f532664d89bba0c2ea00d3c9532e3c2d2
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-26 18:54:24 -0300

    começando exericicio

--- aula 15/index.js
+++ aula 15/index.js
@@ -1,12 +1,16 @@
-let num1 = 9.554554;
+let num1 = 9;
 let num2 = Math.floor(num1);
 let num3 = Math.ceil(num1);
 let num4 = Math.round(num1);
 console.log(num2, num3, num4);
+console.log(num1**(1/2));
 
 
-console.log(Math.max(1,2,3,4,5,6,-10,231,323,322,-140,1556));
-console.log(Math.min(1,2,3,4,5,6,-10,231,323,322,-140,1556));
+console.log(Math.max(1,2,3,4,5,6,-10,231,323,322,-140,1556));//acha o maior
+console.log(Math.min(1,2,3,4,5,6,-10,231,323,322,-140,1556));//acha o menor
 
-const aleatorio = Math.round(Math.random()*(10));
+const aleatorio = Math.round(Math.random()*(10));//aleatorio
 console.log(aleatorio);
+
+console.log(Math.pow(2,10));//elevado
+console.log(2**10); //elevado tbm
