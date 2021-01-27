febe2f48c7744890e5706e047439c58f24d1d1c1
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-27 18:35:05 -0300

    Update index.js

--- aula 17/index.js
+++ aula 17/index.js
@@ -13,7 +13,7 @@ function soma (x,y){
     return resultado;
 }
 console.log(soma(2,2));
-*/
+
 const raiz = function (n){
  return n** 0.5;
 };
@@ -23,3 +23,12 @@ console.log(raiz(9));
 console.log(raiz(25));
 
 console.log(raiz(125 ));
+
+*/
+
+const raiz = n => n ** 0.5;
+console.log(raiz(9));
+
+console.log(raiz(25));
+
+console.log(raiz(111 ));
