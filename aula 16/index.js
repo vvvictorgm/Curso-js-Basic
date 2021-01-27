f107b3e21797937112c6976f090b4d0d66a13427
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-27 18:20:15 -0300

    Update index.js

--- aula 16/index.js
+++ aula 16/index.js
@@ -1,18 +1,24 @@
 const alunos = ['Luiz', 'Maria', 'João'];
 
-delete alunos[1]; //remove o elemento
+alunos.push('Pedrinho');
+alunos.push('Ronaldo');
+
+console.log(alunos.slice(0,3));//vai de 0 até o 3
+console.log(alunos.slice(0,-1));//vai de 0 até todos menos 1
+
+/*delete alunos[1]; //remove o elemento
 const removecomeco = alunos.shift(); //remove o primeiro
 const removido = alunos.pop();//remove o ultimo elemento
 alunos.unshift('Luiza'); //adiciona no começo
-/*alunos[0] = 'Eduardo'; //altera
+alunos[0] = 'Eduardo'; //altera
 alunos[3] = 'Luiza'; //adiciona no indice
 alunos[alunos.length] = 'Pedro'; //adiciona no fim
 alunos[alunos.length] = 'Ronaldo';
 alunos.push('Victor'); //também adiciona no fim
 
 
-*/
+
 console.log(removecomeco);
 console.log(removido);
 console.log(alunos);
-
+*/
