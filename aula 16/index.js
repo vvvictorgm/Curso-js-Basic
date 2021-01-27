7fbab5b5027309805830a10c7923e93758dcfe0c
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-27 17:58:48 -0300

    Update index.js

--- aula 16/index.js
+++ aula 16/index.js
@@ -1,5 +1,12 @@
 const alunos = ['Luiz', 'Maria', 'João'];
 
-alunos[0] = 'Eduardo';
-alunos[3] = 'Luiza';
+alunos[0] = 'Eduardo'; //altera
+alunos[3] = 'Luiza'; //adiciona no indice
+alunos[alunos.length] = 'Pedro'; //adiciona no fim
+alunos[alunos.length] = 'Ronaldo';
+alunos.push('Victor'); //também adiciona no fim
+
+
+
 console.log(alunos);
+
