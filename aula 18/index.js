272af246445ebd309a662d76c7f70b8c4c723af3
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-27 18:42:31 -0300

    Update index.js

--- aula 18/index.js
+++ aula 18/index.js
@@ -1,5 +1,7 @@
-const pessoa = {
- nome:'Luiz',
- sobrenome: 'Miranda',
- idade: 25,
-};
+function criaPessoa(nome,sobrenome,idade){
+    return {nome, sobrenome,idade,};
+    }
+
+    const pessoa1 = criaPessoa('Pedro', 'Ribeiro', 13);
+
+    console.log(pessoa1.nome,pessoa1.idade,pessoa1.sobrenome);
