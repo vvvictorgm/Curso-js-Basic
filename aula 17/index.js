002e7ea5816590c0b9e86f26f8d0eab7944f0e9a
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-27 18:34:15 -0300

    Update index.js

--- aula 17/index.js
+++ aula 17/index.js
@@ -1,4 +1,4 @@
-function saudacao(nome){
+/*function saudacao(nome){
     console.log(`Bom dia ${nome}`);
     return 12345;
 }
@@ -6,3 +6,20 @@ function saudacao(nome){
 saudacao('Victor');
 const variavel = saudacao('Victor');
 console.log(variavel);
+
+
+function soma (x,y){
+    const resultado = x+y;
+    return resultado;
+}
+console.log(soma(2,2));
+*/
+const raiz = function (n){
+ return n** 0.5;
+};
+
+console.log(raiz(9));
+
+console.log(raiz(25));
+
+console.log(raiz(125 ));
