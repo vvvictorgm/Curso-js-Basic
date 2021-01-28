38f2213e82a2ec59ac45f10f3d0830e6c522abbe
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-28 19:35:24 -0300

    teste

--- aula 19/exercicio/js/script.js
+++ aula 19/exercicio/js/script.js
@@ -3,6 +3,9 @@
 //nossas variaveis
 function meuEscopo() {
     const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+    let contador = 0;
+    const pessoas = [];
     /*form.onsubmit = function(evento) {
         //faz com que qualquer função padrão seja desativada, nesse caso estamos usando para que não seja atualizado a pagina
         evento.preventDefault();
@@ -10,15 +13,24 @@ function meuEscopo() {
         console.log("foi enviado");
     };
     */
-   //o efeito é o mesmo, mas agora estamos adicionando um Listener
-   form.addEventListener('submit', function(evento){
-       evento.preventDefault();
-       const nome = form.querySelector('.nome');
-       const nome = form.querySelector('.sobrenome');
-       const nome = form.querySelector('.peso');
-       const nome = form.querySelector('.altura');   
-       console.log("foi enviado");  
-  
-   });
+    //o efeito é o mesmo, mas agora estamos adicionando um Listener
+    form.addEventListener('submit', function (evento) {
+
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const idade = form.querySelector('.idade');
+        const pessoa = {
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            idade: idade.value
+        };
+        console.log(pessoa);
+        pessoas[contador] = pessoa;
+        contador++;
+        resultado.innerHTML += `<p>${nome.value} ${sobrenome.value} altura: ${idade.value} peso: ${peso.value}</p>`;
+    });
 }
 meuEscopo();
