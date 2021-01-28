41686c69a06f147ff091aa38304f23ff4689a5c7
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-28 19:05:04 -0300

    Update script.js

--- aula 19/exercicio/js/script.js
+++ aula 19/exercicio/js/script.js
@@ -1,8 +1,13 @@
 
+//utilizar a função meuEscopo para não poluir o Document e assim preservando 
+//nossas variaveis
 function meuEscopo() {
     const form = document.querySelector('.form');
     form.onsubmit = function(evento) {
+        //faz com que qualquer função padrão seja desativada, nesse caso estamos usando para que não seja atualizado a pagina
+        evento.preventDefault();
         alert(1);
+        console.log("foi enviado");
     };
 }
 meuEscopo();
