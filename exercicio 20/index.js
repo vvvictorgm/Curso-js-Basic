e371ac31e9cd8cbbce05f6e349019abff2dfc79e
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-28 20:50:13 -0300

    Update index.js

--- exercicio 20/index.js
+++ exercicio 20/index.js
@@ -1,10 +1,11 @@
 /*
 Operadores de comparação
->
->=
-<=
-==
-===
-!=
-!==
+> maior que
+>= maior ou igual que
+< menor que
+<= menor ou igual a
+== igualdade (valor)
+=== igual estrita (valor e tipo)
+!= diferente(valor)
+!== diferente estrito (valor e tipo)
 */
