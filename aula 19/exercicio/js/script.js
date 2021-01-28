87828647791b5f5cd302a28f6eeb73912e9ffc11
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-28 19:07:35 -0300

    Update script.js

--- aula 19/exercicio/js/script.js
+++ aula 19/exercicio/js/script.js
@@ -3,11 +3,19 @@
 //nossas variaveis
 function meuEscopo() {
     const form = document.querySelector('.form');
-    form.onsubmit = function(evento) {
+    /*form.onsubmit = function(evento) {
         //faz com que qualquer função padrão seja desativada, nesse caso estamos usando para que não seja atualizado a pagina
         evento.preventDefault();
         alert(1);
         console.log("foi enviado");
     };
+    */
+   //o efeito é o mesmo, mas agora estamos adicionando um Listener
+   form.addEventListener('submit', function(evento){
+       evento.preventDefault();
+       console.log("foi enviado");
+
+       
+   });
 }
 meuEscopo();
