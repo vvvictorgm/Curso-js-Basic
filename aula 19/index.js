1a39e40b633b77af84b1b4d5449c5489569ce7d4
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-28 18:41:50 -0300

    Update index.js

--- aula 19/index.js
+++ aula 19/index.js
@@ -1,3 +1,15 @@
 /*
-Primitivos (imutáveis) - string,number,boolean, undefined, null(bidgint,symbol)
-*/
+Primitivos (imutáveis) - string,number,boolean, 
+undefined, null(bidgint,symbol)*/
+
+let a = 'A';
+let b = a;
+
+console.log(a,b);
+
+a = 'Outra coisa';
+console.log(a,b);
+
+//Referência (mutável) - array, object, funcion
+let c = [1,2,3];
+let d = c;
