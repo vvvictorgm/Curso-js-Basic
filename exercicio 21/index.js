e854eeed28c3f7fddd9eb93a5071431abeb57dca
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-29 17:39:18 -0300

    Update index.js

--- exercicio 21/index.js
+++ exercicio 21/index.js
@@ -7,5 +7,6 @@ Operadores logicos:
 */
 
 const primeiro_exemplo = true && false && true && true && true;
+const segundo_exemplo = false || true || false || false || false;
 
-console.log(primeiro_exemplo);
+console.log(primeiro_exemplo, segundo_exemplo);
