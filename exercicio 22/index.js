ba26f66431200bea4f79218cb8ff6d5edfeefc9a
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-29 18:06:35 -0300

    Update index.js

--- exercicio 22/index.js
+++ exercicio 22/index.js
@@ -3,7 +3,10 @@ Entre 0 -11 - bom dia
 entre 12 - 17 - boa tarde
 entre 18 - 23 - boa noite
  */
- 
+ //if pode ser executado sozinho
+ // sempre que utilizado a palavra else, preciso de um if antes
+ // eu posso ter vários else ifs na checagem
+ //só posso ter um else na checagem
  const hora = 12;
 
 if(hora >= 0 && hora <= 11){
