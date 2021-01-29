b64ef063468ec854930522e0bed950734f621120
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-29 18:05:22 -0300

    Update index.js

--- exercicio 22/index.js
+++ exercicio 22/index.js
@@ -8,4 +8,10 @@ entre 18 - 23 - boa noite
 
 if(hora >= 0 && hora <= 11){
     console.log('Bom dia');
+}else if ( hora >= 12 && hora <= 17){
+    console.log('Boa tarde');
+}else if(hora >= 18 && hora <=23){
+    console.log('Boa noite');
+}else{
+    console.log('Olá');
 }
