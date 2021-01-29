93472062657958a2430083ff943a3e98e067f9cb
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-01-29 17:54:11 -0300

    Update index.js

--- exercicio 21/index.js
+++ exercicio 21/index.js
@@ -10,3 +10,25 @@ const primeiro_exemplo = true && false && true && true && true;
 const segundo_exemplo = false || true || false || false || false;
 
 console.log(primeiro_exemplo, segundo_exemplo);
+
+/*avaliação de curto circuito 
+&& -> false && true -> false " o valor mesmo"
+|| ->
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+Nan
+*/
+
+console.log('Luiz' && NaN && 'Maria');
+
+function falaOi(){
+    return 'Oi';
+}
+
+const vaiExecutar = false;
+
+console.log(vaiExecutar && falaOi());
