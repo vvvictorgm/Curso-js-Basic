ead222fe06024fb13290254e3d79595c60efc594
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-01 19:33:19 -0300

    tudo fucionando

--- aula 23/assets/js/main.js
+++ aula 23/assets/js/main.js
@@ -22,9 +22,23 @@ form.addEventListener('submit', function(evento){
    
 
     const imc = getIMC(peso, altura);
-    console.log(imc);
+    const nivelImc = getNivelIMC(imc);
+    console.log(imc, nivelImc);  
+    const msg = `Seu imc é ${imc} (${nivelImc})`;
+    
+    resultado(msg, true);
 
 });
+
+function getNivelIMC(imc){
+    const nivel = ['Abaixo do peso','Peso normal','Sobrepeso','Obesidade grau 1','Obesidade grau 2','Obesidade grau 3'];
+    if(imc >=39.9) return nivel[5];
+    if(imc >=24.9) return nivel[4];
+    if(imc >=29.9) return nivel[3];
+    if(imc >=24.9) return nivel[2];
+    if(imc >=18.5) return nivel[1];
+    if(imc <18.5) return nivel[0];
+}
 function getIMC(x,y){
     const resultado = x/y**2;
     console.log(resultado);
