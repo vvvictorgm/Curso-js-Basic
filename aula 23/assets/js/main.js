d64de6cad402cde6b0e9fd67adcc944fde3ac17b
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-01 19:17:17 -0300

    arrumado

--- aula 23/assets/js/main.js
+++ aula 23/assets/js/main.js
@@ -6,20 +6,35 @@ form.addEventListener('submit', function(evento){
     resultado("oi");
     const inputPeso = evento.target.querySelector('#peso');
     const inputAltura = evento.target.querySelector('#altura');
-    const peso = Number(inputPeso);
-    const altura = Number(inputAltura);
-
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
+    console.log(peso);
+    console.log(altura);
     if(!peso){
         resultado("Peso Invalido!", false);
+        
         return; 
     }
     if(!altura){
         resultado("Altura Invalida!", false);
         return; 
     }
-    console.log("cheguei aqui");
+   
+
+    const imc = getIMC(peso, altura);
+    console.log(imc);
 
 });
+function getIMC(x,y){
+    const resultado = x/y**2;
+    console.log(resultado);
+    return resultado.toFixed(2);
+}
+
+function criaP(){
+    const p = document.createElement('p');
+    return p;
+}
 
 function resultado (msg, isValid){
     const resultado = document.querySelector('#resultado');
@@ -29,9 +44,5 @@ function resultado (msg, isValid){
     resultado.appendChild(p);
 
 }
-function criaP(){
-    const p = document.createElement('p');
-    return p;
 
-}
 
