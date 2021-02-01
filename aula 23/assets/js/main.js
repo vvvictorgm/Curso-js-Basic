a2f9548e0ff0664624989fe90e3bee557ce3f0e8
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-01 18:21:23 -0300

    cmeçando

---
+++ aula 23/assets/js/main.js
@@ -0,0 +1,10 @@
+function meuEscopo{
+    const peso, altura;
+peso = document.getElementById('peso');
+altura = document.getElementById('altura');
+let imc;
+if()
+imc = (peso/altura)*altura;
+}
+
+meuEscopo();
