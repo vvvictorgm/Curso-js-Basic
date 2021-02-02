6ca389f8366084cca4131c71a4ef8e9bbdbb257b
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-02 20:37:16 -0300

    Update index.js

--- aula 25/index.js
+++ aula 25/index.js
@@ -4,7 +4,14 @@ const data = new Date(0 + tresHoras +umDia); //Timestampunix ou época unix Marc
 
 const data = new Date(2020, 3, 20,15,14,15); //ano, mes, dia, hora, minuto, segundo, mile segundo.
 */
-const data = new Date('2020-02-02 20:20:40');
+const data = new Date('2021-02-02 20:20:40');
 console.log('Dia' , data.getDate());
+console.log('Mês' , data.getMonth()+1); // mes começa do zero
+console.log('Ano' , data.getFullYear());
+console.log('Hora' , data.getHours());
+console.log('Minuto' , data.getMinutes());
+console.log('Segundo' , data.getSeconds());
+console.log('Mile segundo' , data.getMilliseconds());
+console.log('Dia semana' , data.getDay()); // 0 - domingo 6 -sabado
 
 console.log(data.toString());
