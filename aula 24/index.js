e224f378764e64a8331b59ac54ddc08b8ea49bfc
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-02 18:07:36 -0300

    Update index.js

--- aula 24/index.js
+++ aula 24/index.js
@@ -1,8 +1,13 @@
-// ? :
+// (condicao) ? 'Valor verdadeiro' : 'Valo para falso' :
 
 const pontuacaoUsuario = 9929;
 const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
-console.log(nivelUsuario);
+
+const corUsuario = 'verde';
+const corPadrao = corUsuario || 'Preta';
+
+
+console.log(nivelUsuario, corPadrao);
 /*if(pontuacaoUsuario >=1000){
     console.log('usuario VIP');
 }else{
