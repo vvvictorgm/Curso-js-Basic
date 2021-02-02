ff7cc03d867dcf809cc658be19f197fd456ee417
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-02 20:02:07 -0300

    Update index.js

--- aula 25/index.js
+++ aula 25/index.js
@@ -1,3 +1,6 @@
-const tresHoras = 60*60*3*1000; //3horas
-const data = new Date(); //Timestampunix ou época unix Marco zero = 01/01/1970
+/*const tresHoras = 60*60*3*1000; //3horas
+const umDia = 60*60*24*1000;
+const data = new Date(0 + tresHoras +umDia); //Timestampunix ou época unix Marco zero = 01/01/1970
+*/
+const data = new Date(2020, 3, 20,15,14,15); //ano, mes, dia, hora, minuto, segundo, mile segundo.
 console.log(data.toString());
