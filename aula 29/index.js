26ac4f6675b6c8df359092f80609e614a0b54e04
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-04 19:26:28 -0300

    Update index.js

--- aula 29/index.js
+++ aula 29/index.js
@@ -1,3 +1,4 @@
 const numeros = [1,2,3,4,5,6,7,8,9];
-const [primeiroNumero, segundoNumero] = numeros;
-console.log(primeiroNumero,segundoNumero);
+const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros;
+console.log(primeiroNumero,segundoNumero, terceiroNumero);
+console.log(resto);
