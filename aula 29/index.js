70d623b22e5747d038848020120f51282c7ba160
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-04 19:35:48 -0300

    Update index.js

--- aula 29/index.js
+++ aula 29/index.js
@@ -1,6 +1,14 @@
 //...rest, ...spread;
 
-const numeros = [1,2,3,4,5,6,7,8,9];
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const [primeiroNumero, , terceiroNumero, , quintoNumero, ...resto] = numeros;
-console.log(primeiroNumero,terceiroNumero, quintoNumero);
+console.log(primeiroNumero, terceiroNumero, quintoNumero);
 console.log(resto);
+
+//                              0           1           2
+//                           0  1  2    0  1  2     0  1  2
+
+const arrayDentroDeArray = [[1, 2, 3], [4, 5, 6,], [7, 8, 9]];
+const[,,[,oito]] = arrayDentroDeArray;
+console.log(oito);
+console.log(arrayDentroDeArray[1][2]);//busca o elemento 2 dentro do elemento 1 = 6 
