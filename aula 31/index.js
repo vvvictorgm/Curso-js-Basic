645c0155b47dc55aa8b90a462fe1483b7dcb1fdf
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-04 20:31:01 -0300

    Update index.js

--- aula 31/index.js
+++ aula 31/index.js
@@ -1,10 +1,3 @@
-
-
-for(let i=1000; i > 600; i -= 10){
-    console.log(`Linha ${i}`);
-
-}
-
 for (let i = 0; i<=10;i++){
     const par = i % 2 === 0;
     console.log(i,par);
@@ -13,4 +6,10 @@ for (let i = 0; i<=10;i++){
 for(let i = 0; i<=10; i++){
     const parOuImpar = i%2===0 ? 'par' : 'Impar';
     console.log(i, parOuImpar);
+}
+
+const frutas = ['Maçã','Pera','Uva', 'Banana', 'Romã'];
+for(let i = 0; i == frutas.length, i++){
+
+    
 }
