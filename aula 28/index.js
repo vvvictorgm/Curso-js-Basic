45aa79ade9ce509d40796876fd8a34dbf06a8d2e
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-04 19:03:07 -0300

    Create index.js

---
+++ aula 28/index.js
@@ -0,0 +1,12 @@
+const verdadeira = true;
+
+//let tem escopo de bloco {....bloco}
+//var só tem escorpo de função
+
+let nome ='Luiz';
+var nome2= 'Luiz';
+
+if(verdadeira){
+    let nome = 'Otavio';
+    console.log(nome,nome2);
+}
