c93b73fc7b1d48bf042cb9cbdf078d3f93140909
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-04 19:27:18 -0300

    Update index.js

--- aula 29/index.js
+++ aula 29/index.js
@@ -1,3 +1,5 @@
+//...rest, ...spread;
+
 const numeros = [1,2,3,4,5,6,7,8,9];
 const [primeiroNumero, segundoNumero, terceiroNumero, ...resto] = numeros;
 console.log(primeiroNumero,segundoNumero, terceiroNumero);
