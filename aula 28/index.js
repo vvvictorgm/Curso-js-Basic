aee8b12a8a5af4055fceec1b5fb7d99e6414ec22
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-04 19:11:35 -0300

    Update index.js

--- aula 28/index.js
+++ aula 28/index.js
@@ -3,15 +3,18 @@ const verdadeira = true;
 //let tem escopo de bloco {....bloco}
 //var só tem escorpo de função
 
-let nome ='Luiz';
-var nome2= 'Luiz';
+let nome ='Luiz';//criando
+var nome2= 'Luiz';//criando
 
 if(verdadeira){
-    let nome = 'Otavio';
+    let nome = 'Otavio';//criando
+    var nome2 = 'Pedro'; //redeclarando
     console.log(nome,nome2);
 
     if(verdadeira){
-        let nome = 'Outra coisa';
+        let nome = 'Outra coisa';//criando
+        var nome2 = 'Ronaldo';//redeclarando
         console.log(nome,nome2);
     }
 }
+console.log(nome,nome2);
