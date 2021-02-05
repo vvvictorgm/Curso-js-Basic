120eecb4fbfb2b25cadfa61267d373a6ecfd6bd9
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-05 19:59:28 -0300

    Update main.js

--- Exercicio 32/assets/js/main.js
+++ Exercicio 32/assets/js/main.js
@@ -5,10 +5,13 @@ const elementos =[
     {tag: 'footer',texto:'Frase3'},
     {tag: 'section',texto:'Frase4'},
 ];
+//até que o elemento do array chegue no fim, iremos execultar o loop
 for(i = 0; i <= elementos.length; i++){
+    //uma variavel para capturar a tag
     let tag = elementos[i].tag;
+    //uma variavel para capturar o texto
     let texto = elementos[i].texto;
-    console.log(tag,texto);
+    //para cada vez que rodarmos o loop, iremos inserir a tag e o texto dentro do escopo
     div.innerHTML += `<${tag}> ${texto}</${tag}>`;
 }
 
