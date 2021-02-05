105f3e98fb776a9c9cae748041694d43210ced69
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-05 19:40:42 -0300

    Update main.js

--- Exercicio 32/assets/js/main.js
+++ Exercicio 32/assets/js/main.js
@@ -1,7 +1,8 @@
 const div = querrySelector('.divPrincipal');
 const elementos =[
-    {tag: 'p',texto:'Frase1'}
-    {tag: 'div',texto:'Frase2'}
-    {tag: 'footer',texto:'Frase3'}
-    {tag: 'section',texto:'Frase4'}
-]
+    {tag: 'p',texto:'Frase1'},
+    {tag: 'div',texto:'Frase2'},
+    {tag: 'footer',texto:'Frase3'},
+    {tag: 'section',texto:'Frase4'},
+];
+
