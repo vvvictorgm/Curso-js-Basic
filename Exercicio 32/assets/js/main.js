230778db90729f2996ccdcd94a54ea9830f81403
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-05 19:43:57 -0300

    Update main.js

--- Exercicio 32/assets/js/main.js
+++ Exercicio 32/assets/js/main.js
@@ -1,8 +1,13 @@
-const div = querrySelector('.divPrincipal');
+const div = document.querySelector('.divPrincipal');
 const elementos =[
     {tag: 'p',texto:'Frase1'},
     {tag: 'div',texto:'Frase2'},
     {tag: 'footer',texto:'Frase3'},
     {tag: 'section',texto:'Frase4'},
 ];
+for(i = 0; i <= elementos.length; i++){
+    let inserir = elementos.i;
+    console.log(inserir);
+//div.innerHTML();
+}
 
