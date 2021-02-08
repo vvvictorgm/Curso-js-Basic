83e4044b9371fb553224e148668417afcb29434b
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 15:55:04 -0300

    Update index.js

--- aula 37/index.js
+++ aula 37/index.js
@@ -11,3 +11,21 @@ for (let numero of numeros){
     }
     console.log(numero);
 }
+
+let i = 0;
+
+while (i<numeros.length){
+
+    if (i === 2){
+        i++;
+        continue;
+    }
+
+    if(i === 7 ){
+        i++;
+        break;
+    }
+    console.log(i);
+    i++;
+
+}
