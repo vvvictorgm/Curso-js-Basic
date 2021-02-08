d3a60c28649e5c4ef91f59e8b3b798dbea885eb3
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 16:00:51 -0300

    Update index.js

--- exercicio 38/index.js
+++ exercicio 38/index.js
@@ -1,1 +1,13 @@
 //Escreva uma função que receba dois numeros e retorn o maior deles
+
+function maior (a,b){
+    const n1 = a;
+    const n2 = b;
+    if(n1===n2){
+        return `São iguais`;
+    }
+    const resultado = n1 > n2?n1 :n2 ;
+    return(resultado);
+}
+
+console.log(maior(34,34));
