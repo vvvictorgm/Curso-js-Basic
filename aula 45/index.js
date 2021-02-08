aca84320de9da99292c28b42b11395cd89be184d
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 18:44:02 -0300

    Update index.js

--- aula 45/index.js
+++ aula 45/index.js
@@ -1,7 +1,12 @@
 try{
     //É executado quando não há erros
+    console.log('Abri um arquivo');
+    console.log('Manipulei o arquivo e deu erro');
+    console.log('Fechei o arquivo');
 }catch(e){
     // É executado quando há erros
+    console.log('tratei um erro');
 }finally{
     //é sempre executado
+    console.log('sou sempre executado');
 }
