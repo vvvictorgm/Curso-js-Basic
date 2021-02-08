4c5f991b150fcfbf6cbd2fa5cec6921ec8f9d26c
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 18:33:25 -0300

    Update index.js

--- aula 41/index.js
+++ aula 41/index.js
@@ -2,12 +2,16 @@
 
  function soma(x,y){
 if(typeof x !== 'number' || typeof y !== 'number'){
- throw('x e y precisam ser numeros');
+ throw new Error ('x e y precisam ser numeros');
 }
   return x+y;    
  }
-
+ try{
  console.log(soma(1,3));
+ console.log(soma('1',2));
+}catch(e){
+    console.log(e);
+}
 
 /*
 try{
