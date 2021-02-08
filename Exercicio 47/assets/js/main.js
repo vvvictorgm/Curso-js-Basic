d4860272ce00fd651310d958e058ec6d33bebb57
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 20:33:34 -0300

    Update main.js

--- Exercicio 47/assets/js/main.js
+++ Exercicio 47/assets/js/main.js
@@ -2,18 +2,36 @@ const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+const timer = transformarEmHora();
+timer.setHours(3,3,3);
 
-iniciar.addEventListener('click', function{
+function transformarEmHora (){
+    let data = new Date();
+    data.setHours(0,0,0);
+    
+    return data.toLocaleTimeString('pt-BR', {
+        hour12:false
+    });
+}
+
+
+
+
+
+iniciar.addEventListener('click', function(evento){
+    setInterval(function(){
+        
+        relogio.innerHTML(timer);
+    },1000);  
 
 
 });
 
-pausar.addEventListener('click', function{
+pausar.addEventListener('click', function(evento){
 
+});
+
+zerar.addEventListener('click', function(evento){
 
 });
 
-zerar.addEventListener('click', function{
-
-
-});
