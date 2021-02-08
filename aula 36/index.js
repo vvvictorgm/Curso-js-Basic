7db4a5d74b62be8731a11ee2b43f4d01105c13f4
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 15:25:36 -0300

    infinitooooooooo

--- aula 36/index.js
+++ aula 36/index.js
@@ -1,6 +1,18 @@
-let i = 0;
+/*let i = 0;
 
 while (i <=10){
     console.log(i);
     i++;
+}
+*/
+
+function random(min,max){
+    const r = Math.random() *(max-min)+min;
+    return Math.floor(r);
+
+}
+
+let rand = random(1,50);
+while(rand !== 10){
+console.log(rand);
 }
