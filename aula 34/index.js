4fb18d41d43dddb3afea86391ae7425a84a6732d
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 14:30:33 -0300

    Update index.js

--- aula 34/index.js
+++ aula 34/index.js
@@ -1,4 +1,19 @@
 const nome = 'Victor Augusto';
+const nomes = ['Victor', 'Augusto', 'Broot'];
+//for of
+for (let valor of nome){
+    console.log(valor);
+}
+
+for(let i of nomes){
+    console.log(i);
+}
+nomes.forEach(function(valor, indice){
+    console.log(valor, indice);
+});
+
+
+/*
 //for normal
 for(let i = 0; i< nome.length; i++){
     console.log(nome[i]);
@@ -7,3 +22,4 @@ for(let i = 0; i< nome.length; i++){
 for (let i in nome){
     console.log(nome[i]);
 }
+*/
