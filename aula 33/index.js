56374e1dabf94ddc5ae6b1bb781df8e9d6424c00
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 14:23:48 -0300

    Update index.js

--- aula 33/index.js
+++ aula 33/index.js
@@ -1,5 +1,19 @@
+// For in -> lê os indices ou chaves do objeto
 const frutas = ['Pera', 'Maçã', 'Uva'];
 
 for (let i in frutas){
     console.log(frutas[i]);
+}
+
+const pessoa ={
+    nome:'Luiz', 
+    sobrenome: 'Otavio',
+    idade: 30
+};
+
+for (let chaves in pessoa){
+    console.log(chaves);
+}
+for (let dados in pessoa){
+    console.log(pessoa[dados]);
 }
