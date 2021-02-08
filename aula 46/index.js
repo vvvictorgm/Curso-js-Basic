5550d0be3c6e72221a7f1fed5c7b82f5ca9d2b7b
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 19:07:21 -0300

    Update index.js

--- aula 46/index.js
+++ aula 46/index.js
@@ -6,7 +6,11 @@ function mostrarHora(){
     });
 
 }
-function funcaoDoIntervalo(){
+
+const timer = setInterval(function(){
     console.log(mostrarHora());
-}
-setInterval();
+}, 1000);
+
+setTimeout(function(){
+    clearInterval(timer);
+}, 10000)
