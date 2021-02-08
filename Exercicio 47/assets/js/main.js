d4f52abe894627e68fe83028d6f03609d1a3d942
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-08 20:12:01 -0300

    Update main.js

--- Exercicio 47/assets/js/main.js
+++ Exercicio 47/assets/js/main.js
@@ -2,3 +2,18 @@ const relogio = document.querySelector('.relogio');
 const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const zerar = document.querySelector('.zerar');
+
+iniciar.addEventListener('click', function{
+
+
+});
+
+pausar.addEventListener('click', function{
+
+
+});
+
+zerar.addEventListener('click', function{
+
+
+});
