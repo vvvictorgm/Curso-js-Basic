c538673204649bcc15f73b8d9dce89afab43bd3e
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 11:51:50 -0300

    começando2

--- exercicio 48/assets/js/main.js
+++ exercicio 48/assets/js/main.js
@@ -1,3 +1,10 @@
 const novaTarefa = document.querySelector('.nova-tarefa');
 const botaoAadicionarTarefa = document.querySelector('.botao-adicionar-tarefa');
 const tarefas = document.querySelector('.tarefas');
+
+botaoAadicionarTarefa.addEventListener('click', function(){
+    if(!novaTarefa.value) return;
+    
+    console.log(novaTarefa.value);
+
+});
