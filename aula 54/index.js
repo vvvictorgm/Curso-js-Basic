8aa34c217e03a7d6977245f33aef9d3b8e30bfcf
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 19:43:40 -0300

    Update index.js

--- aula 54/index.js
+++ aula 54/index.js
@@ -1,27 +1,32 @@
-function random (min = 1000, max = 3000){
-    const aleatorio = Math.random()*(max - min )+min;
+function random(min = 1000, max = 3000) {
+    const aleatorio = Math.random() * (max - min) + min;
     return Math.floor(aleatorio);
 }
-function f1() {
-    setTimeout(function (callback) {
+function f1(callback) {
+    setTimeout(function () {
         console.log('f1');
-        if(callback)callback();
+        if (callback) callback();
     }, random());
 }
-function f2() {
-    setTimeout(function (callback) {
+function f2(callback) {
+    setTimeout(function () {
         console.log('f2');
-        if(callback)callback();
+        if (callback) callback();
     }, random());
 }
-function f3() {
-    setTimeout(function (callback) {
+function f3(callback) {
+    setTimeout(function () {
         console.log('f3');
-        if(callback)callback();
+        if (callback) callback();
     }, random());
 }
 
-f1();
-f2();
-f3();
-console.log('Ola Mundo');
+f1(function () {
+    f2(function () {
+        f3(function () {
+            console.log('Ola Mundo');
+        });
+    });
+});
+ 
+
