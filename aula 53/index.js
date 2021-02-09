dc7e306937cbb1e952a1018542068da0c87b5b02
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 19:30:53 -0300

    Update index.js

--- aula 53/index.js
+++ aula 53/index.js
@@ -1,6 +1,13 @@
-function criaOutraFuncao(){
-    const   nome = 'Luiz';
+function criaOutraFuncao(nome){
+
     return function(){
         return  nome;
     }
 }
+
+const funcao = criaOutraFuncao('Victor');
+const funcao2 = criaOutraFuncao('Pedro');
+console.dir(funcao);
+console.dir(funcao2);
+console.log(funcao());
+console.log(funcao2());
