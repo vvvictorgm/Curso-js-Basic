f42f0fd8fb0af05e3b41ad6b51ee81e8fe633551
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 16:29:22 -0300

    Update index.js

--- aula 50/index.js
+++ aula 50/index.js
@@ -2,3 +2,13 @@ function funcao(){
 console.log(arguments);
 }
 funcao('Valor',1,2,3,4,5,6,7,8,9);
+
+function soma(){
+    let total = 0;
+    for(let argumento of arguments){
+        total += argumento;
+    }
+    console.log(total)
+}
+
+soma(1,23,4,5,6,7,44,43,3,45,6,3);
