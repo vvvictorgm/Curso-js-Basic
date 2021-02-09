58add33550df5d8b775d379303fe5c78eece4b7e
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 19:33:58 -0300

    Update index.js

--- aula 54/index.js
+++ aula 54/index.js
@@ -1,11 +1,17 @@
-function f1(){
-    console.log('f1');
+function f1() {
+    setTimeout(function () {
+        console.log('f1');
+    }, 8000);
 }
-function f2(){
-    console.log('f2');
+function f2() {
+    setTimeout(function () {
+        console.log('f2');
+    }, 1000);
 }
-function f2(){
-    console.log('f2');
+function f3() {
+    setTimeout(function () {
+        console.log('f3');
+    }, 3000);
 }
 
 f1();
