fac9f960532f120a9140510ff2f3b445c9992488
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 19:39:34 -0300

    Update index.js

--- aula 54/index.js
+++ aula 54/index.js
@@ -3,18 +3,21 @@ function random (min = 1000, max = 3000){
     return Math.floor(aleatorio);
 }
 function f1() {
-    setTimeout(function () {
+    setTimeout(function (callback) {
         console.log('f1');
+        if(callback)callback();
     }, random());
 }
 function f2() {
-    setTimeout(function () {
+    setTimeout(function (callback) {
         console.log('f2');
+        if(callback)callback();
     }, random());
 }
 function f3() {
-    setTimeout(function () {
+    setTimeout(function (callback) {
         console.log('f3');
+        if(callback)callback();
     }, random());
 }
 
