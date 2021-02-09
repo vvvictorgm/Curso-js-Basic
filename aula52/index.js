9814a0d82286f9c1d70d55e09e6fc22d96bbccac
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 19:14:03 -0300

    Update index.js

--- aula52/index.js
+++ aula52/index.js
@@ -1,3 +1,15 @@
-function duplica(){
-    return n*2;
+
+
+/*function duplica(n) {
+    return n * 2;
 }
+function triplica(n) {
+    return n * 3;
+}
+function quadriplica(n) {
+    return n * 4;
+}
+*/
+console.log(duplica(2));
+console.log(triplica(2));
+console.log(quadriplica(2));
