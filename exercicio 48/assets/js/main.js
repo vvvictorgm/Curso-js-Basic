2632fd873fec5f6d6ae93e99ef11e51858105114
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-09 11:48:18 -0300

    Update main.js

--- exercicio 48/assets/js/main.js
+++ exercicio 48/assets/js/main.js
@@ -1,1 +1,3 @@
 const novaTarefa = document.querySelector('.nova-tarefa');
+const botaoAadicionarTarefa = document.querySelector('.botao-adicionar-tarefa');
+const tarefas = document.querySelector('.tarefas');
